299d0b0de30b6ba9bfc1eb693ab20bf9d3054009
uhkay <konstantius.kevin@gmail.com>
2025-02-25 17:54:25 +0700

    update routes

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -18,9 +20,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d", id)
-
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +28,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+
 	w.Write([]byte("Save a new snippet"))
 }
 
